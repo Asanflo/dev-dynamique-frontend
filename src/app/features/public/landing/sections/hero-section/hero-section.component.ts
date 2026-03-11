067b00ff27a9a7f2ad2c4451e31ed72e1aefaeb7
Agassem Sanda Florentin <agassemflorentin@gmail.com>
2026-03-11 20:50:30 +0100

    defining hero section

--- src/app/features/public/landing/sections/hero-section/hero-section.component.ts
+++ src/app/features/public/landing/sections/hero-section/hero-section.component.ts
@@ -1,8 +1,10 @@
 import {Component} from '@angular/core';
+import {ScrollReveal} from '../../../../../directives/scroll-reveal';
 
 @Component({
   selector: 'app-hero-section',
   standalone: true,
+  imports: [ScrollReveal],
   templateUrl: 'hero-section.component.html',
   styleUrl: 'hero-section.component.css'
 })
