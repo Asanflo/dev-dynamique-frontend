83c887cac8079368421412ec37ad26e099ddc175
Agassem Sanda Florentin <agassemflorentin@gmail.com>
2026-03-14 00:47:05 +0100

    defining testimonies section

--- src/app/features/public/landing/landing.component.ts
+++ src/app/features/public/landing/landing.component.ts
@@ -2,11 +2,12 @@ import {Component} from '@angular/core';
 import {HeroSectionComponent} from './sections/hero-section/hero-section.component';
 import {ProblemSectionComponent} from './sections/problem-section/problem-section.component';
 import {FeaturesSectionComponent} from './sections/features-section/features-section.component';
+import {TestimonialSectionComponent} from './sections/testimonials-section/testimonial-section.component';
 
 @Component({
   selector:'app-landing',
   standalone: true,
-  imports: [HeroSectionComponent, ProblemSectionComponent, FeaturesSectionComponent],
+  imports: [HeroSectionComponent, ProblemSectionComponent, FeaturesSectionComponent, TestimonialSectionComponent],
   templateUrl: 'landing.component.html'
 })
 
--- src/app/features/public/landing/sections/testimonials-section/testimonial-section.component.ts
+++ src/app/features/public/landing/sections/testimonials-section/testimonial-section.component.ts
@@ -1,11 +1,16 @@
 import {Component} from '@angular/core';
+import {TestiCardComponent} from '../../../../../shared/testi-card/app-testi-card';
+import {ScrollReveal} from '../../../../../directives/scroll-reveal';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-testimonial-section',
   standalone: true,
+  imports: [TestiCardComponent, ScrollReveal, FontAwesomeModule],
   templateUrl:'testimonial-section.component.html'
 })
 
 export class TestimonialSectionComponent{
-
+  faStar = faStar;
 }
