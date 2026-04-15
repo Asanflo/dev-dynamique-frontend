b7cf94e549936c31b1590f989147c014766bfaed
Agassem Sanda Florentin <agassemflorentin@gmail.com>
2026-04-15 18:20:53 +0100

    feat: about us

--- src/app/features/public/landing/landing.component.ts
+++ src/app/features/public/landing/landing.component.ts
@@ -3,11 +3,12 @@ import {HeroSectionComponent} from './sections/hero-section/hero-section.compone
 import {ProblemSectionComponent} from './sections/problem-section/problem-section.component';
 import {FeaturesSectionComponent} from './sections/features-section/features-section.component';
 import {TestimonialSectionComponent} from './sections/testimonials-section/testimonial-section.component';
+import {AboutSectionComponent} from './sections/about-section/about-section.component';
 
 @Component({
   selector:'app-landing',
   standalone: true,
-  imports: [HeroSectionComponent, ProblemSectionComponent, FeaturesSectionComponent, TestimonialSectionComponent],
+  imports: [HeroSectionComponent, ProblemSectionComponent, FeaturesSectionComponent, TestimonialSectionComponent, AboutSectionComponent],
   templateUrl: 'landing.component.html'
 })
 
---
+++ src/app/features/public/landing/sections/about-section/about-section.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+import {ScrollReveal} from '../../../../../directives/scroll-reveal';
+
+
+@Component({
+  selector: 'app-about-section',
+  imports: [ScrollReveal],
+  standalone: true,
+  templateUrl: 'about-section.component.html'
+})
+
+export class AboutSectionComponent{
+
+}
